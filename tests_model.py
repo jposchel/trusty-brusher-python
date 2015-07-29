2f8dcd25a31355a20b18f86318a57dd8a62120d4
jposchel <jposc539@gmail.com>
2015-07-29 19:40:29 -0400

    Finished Tooth class unit tests - for now

--- tests_model.py
+++ tests_model.py
@@ -12,6 +12,17 @@ def test_tooth_class():
     """
     try:
         tooth_1 = Tooth(1, False, 0)
+        assert tooth_1.get_location() == 1
+        assert tooth_1.get_cavity_status() == False
+        assert tooth_1.get_germ_count() == 0
+
+        tooth_1.set_location(21)
+        tooth_1.set_cavity_status(True)
+        tooth_1.set_germ_count(10)
+        assert tooth_1.get_location() == 21
+        assert tooth_1.get_cavity_status() == True
+        assert tooth_1.get_germ_count() == 10
+
         print("All Tooth class tests pass!")
     except:
         e = sys.exc_info()[0]
