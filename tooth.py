a39d377ccf1801c8fdfabbc48b717b77c88d84b2
jposchel <jposc539@gmail.com>
2015-07-29 22:01:14 -0400

    Changed arg variable names in tooth.py to new personal convention

--- tooth.py
+++ tooth.py
@@ -11,27 +11,27 @@ class Tooth:
     
     """
     
-    def __init__(self, new_location, new_cavity_status, new_germ_count):
-        self.location = new_location
-        self.has_cavity = new_cavity_status
-        self.germ_count = new_germ_count
+    def __init__(self, init_location, init_cavity_status, init_germ_count):
+        self.location = init_location
+        self.has_cavity = init_cavity_status
+        self.germ_count = init_germ_count
 
     def get_location(self):
         return self.location
 
-    def set_location(self, new_loc):
-        self.location = new_loc
+    def set_location(self, new_location):
+        self.location = new_location
 
     def get_cavity_status(self):
         return self.has_cavity
 
-    def set_cavity_status(self, new_status):
-        self.has_cavity = new_status
+    def set_cavity_status(self, new_cavity_status):
+        self.has_cavity = new_cavity_status
 
     def get_germ_count(self):
         return self.germ_count
 
-    def set_germ_count(self, new_count):
-        self.germ_count = new_count
+    def set_germ_count(self, new_germ_count):
+        self.germ_count = new_germ_count
 
 
