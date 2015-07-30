5a298f887d02ffa3e7eff17603e2d7895f9ca55b
jposchel <jposc539@gmail.com>
2015-07-29 23:18:06 -0400

    Added unit tests for Germ class in tests_model.py

--- tests_model.py
+++ tests_model.py
@@ -4,6 +4,7 @@
 
 import sys
 from tooth import Tooth
+from germ import Germ
 
 def test_tooth_class():
     """
@@ -28,8 +29,40 @@ def test_tooth_class():
         e = sys.exc_info()[0]
         print("Caught an error: %s" % e)
 
+def test_germ_class():
+    """
+       Test Germ class.
+
+    """
+    try:
+        germ_1 = Germ(52, 10)
+        assert germ_1.get_location() == 52
+        assert germ_1.get_health() == 10
+        
+        germ_1.set_location(103)
+        germ_1.set_health(2)
+        assert germ_1.get_location() == 103
+        assert germ_1.get_health() == 2
+
+        germ_1.increment_health()
+        assert germ_1.get_health() == 3
+
+        germ_1.decrement_health()
+        germ_1.decrement_health()
+        germ_1.decrement_health()
+        assert germ_1.get_health() == 0
+
+        germ_1.decrement_health()
+        assert germ_1.get_health() == 0
+
+        print("All Germ class tests pass!")
+    except:
+        e = sys.exc_info()[0]
+        print("Caught an error: %s" % e)
+
 def main():
     test_tooth_class()
+    test_germ_class()
 
 if __name__ =='__main__':
     main()
