61743526a9fbd1545b20853abe349e2f3e69e187
jposchel <jposc539@gmail.com>
2015-07-31 16:08:29 -0400

    Implemented the rest of the functions in Person class

--- person.py
+++ person.py
@@ -35,49 +35,86 @@ class Person:
         self.is_flossing = flossing_state
 
     def be_alert(self):
-
+        self.__change_action_states(True, False, False, False, False, False)
 
     def wake_up(self):
+        self.be_alert()
 
     def go_to_sleep(self):
+        self.__change_action_states(False, True, False, False, False, False)
 
     def eat(self):
+        self.__change_action_states(True, False, True, False, False, False)
 
     def hurt(self):
+        self.__change_action_states(True, False, False, True, False, False)
 
     def brush_teeth(self):
+        self.__change_action_states(True, False, False, False, True, False)
 
     def floss_teeth(self):
+        self.__change_action_states(True, False, False, False, False, True)
 
     def get_name(self):
+        return self.name
 
     def set_name(self, new_name):
+        self.name = new_name
 
     def get_gender(self):
-
-    def set_gender(self, new_gender):
+        return self.gender
 
     def get_is_awake(self):
+        return self.is_awake
 
     def toggle_awake(self):
+        if self.is_awake == True:
+            self.is_awake = False
+        else:
+            self.is_awake = True
 
     def get_is_sleeping(self):
+        return self.is_sleeping
 
     def toggle_sleeping(self):
+        if self.is_sleeping == True:
+            self.is_sleeping = False
+        else:
+            self.is_sleeping = True
 
     def get_is_eating(self):
+        return self.is_eating
 
     def toggle_eating(self):
+        if self.is_eating == True:
+            self.is_eating = False
+        else:
+            self.is_eating = True
 
     def get_is_hurting(self):
+        return self.is_hurting
 
     def toggle_hurting(self):
+        if self.is_hurting == True:
+            self.is_hurting = False
+        else:
+            self.is_hurting = True
 
     def get_is_brushing(self):
+        return self.is_brushing
 
     def toggle_brushing(self):
+        if self.is_brushing == True:
+            self.is_brushing = False
+        else:
+            self.is_brushing = True
 
     def get_is_flossing(self):
+        return self.is_flossing
 
     def toggle_flossing(self):
+        if self.is_flossing == True:
+            self.is_flossing = False
+        else:
+            self.is_flossing = True
     
