0f3d748710b8f5dc192e48b53e819e7f3d690e8f
jposchel <jposc539@gmail.com>
2015-07-31 14:58:16 -0400

    Added private __change_person_states() function to Person class

--- person.py
+++ person.py
@@ -24,8 +24,18 @@ class Person:
     	self.is_hurting = False
     	self.is_brushing = False
     	self.is_flossing = False
-
+    
+    def __change_person_states(self, awake_state, sleeping_state, eating_state,
+      hurting_state, brushing_state, flossing_state):
+        self.is_awake = awake_state
+        self.is_sleeping = sleeping_state
+        self.is_eating = eating_state
+        self.is_hurting = hurting_state
+        self.is_brushing = brushing_state
+        self.is_flossing = flossing_state
+    
     def be_alert(self):
+
 
     def wake_up(self):
 
