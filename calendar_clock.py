642879cf82df73a7493517bfb920cef0942898ee
jposchel <jposc539@gmail.com>
2015-08-10 18:09:30 -0400

    Finished Calender_Clock class constructor

--- calendar_clock.py
+++ calendar_clock.py
@@ -2,3 +2,14 @@
 
 """calendar_clock.py: Public Calendar Clock class representing a time in the educational game Trusty Brusher."""
 
+import threading
+import time
+
+class Calendar_Clock(threading.Thread):
+    def __init__(self, init_start_day="Sunday", init_start_hour=7, init_start_minute=0):
+    	threading.Thread.__init__(self)
+        self.day_list = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+        self.current_day = init_start_day
+        self.current_hour = init_start_hour
+        self.current_minute = init_start_minute
+
