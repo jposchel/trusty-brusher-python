8804293fa57636d1d96dbfd99c3298b8a5f76089
jposchel <jposc539@gmail.com>
2015-08-10 19:32:12 -0400

    Wrote Calendar_Clock class header docs

--- calendar_clock.py
+++ calendar_clock.py
@@ -6,6 +6,32 @@ import threading
 import time
 
 class Calendar_Clock(threading.Thread):
+    """
+       A Calendar Clock that tracks current day of the week, current hour, and current minute.
+       self.day_list: list value indicating names and order of days of the week
+       self.current_day: string value indicating the current day of the week
+       self.current_hour: int value indicating the current hour of the day (24-hour clock is used, 0-23)
+       self.current_minute: int value indicating the current minute of the hour (60 minutes per hour, 0-59)
+
+       To use Calendar_Clock, create an instance and call Python threading module methods on that instance:
+
+           some_new_clock = Calendar_Clock()
+
+           # ... or ...
+
+           some_new_clock = Calendar_Clock("Friday", 17, 5)
+           some_new_clock.start()
+           
+           # ... do some work ...
+           # ... use instance methods defined below ...
+           # ... and when finished ...
+
+           some_new_clock.end()
+
+       Some class design elements taken from Python Advanced Turtorial 5 - MultiThreading
+       by DrapsTV, found here: https://youtu.be/EvbA3qVMGaw
+
+    """
     def __init__(self, init_start_day="Sunday", init_start_hour=7, init_start_minute=0):
     	threading.Thread.__init__(self)
         self.day_list = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
@@ -13,3 +39,5 @@ class Calendar_Clock(threading.Thread):
         self.current_hour = init_start_hour
         self.current_minute = init_start_minute
 
+    def run(self):
+        pass
