728d80f9e3ac8e5ec6b54e6b839f9cf1d5ad094e
jposchel <jposc539@gmail.com>
2015-08-13 22:41:22 -0400

    Added basic stub file for Trusty_Brusher_Game_World class

---
+++ trusty_brusher_game_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""trusty_brusher_game_world.py: Public Game World class representing the educational game Trusty Brusher."""
+
+from calendar_clock import Calendar_Clock
+from food import Food
+from germ import Germ
+from person import Person
+from tooth import Tooth
+
+class Trusty_Brusher_Game_World:
+    def __init__(self):
+        pass
