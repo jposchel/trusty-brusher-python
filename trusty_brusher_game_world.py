c5b17ba5be33ac20ca2c6f9fa0baefd6ece44342
jposchel <jposc539@gmail.com>
2015-08-14 14:26:24 -0400

    Added getter/setter methods for Game_World class

--- trusty_brusher_game_world.py
+++ trusty_brusher_game_world.py
@@ -16,8 +16,29 @@ class Trusty_Brusher_Game_World:
         self.healthy_food = [Food(specs[0], specs[1], specs[2]) for specs in healthy_food_info]
         self.unhealthy_food = [Food(specs[0], specs[1], specs[2]) for specs in unhealthy_food_info]
         self.upcoming_food = []
-        self.tooth_brush = False
-        self.tooth_paste = False
+        self.toothbrush = False
+        self.toothpaste = False
         self.floss = False
-        self.game_over = False
         self.game_paused = False
+        self.game_over = False
+
+    def is_game_paused(self):
+        return self.game_paused
+
+    def pause_game(self, new_pause_status):
+        self.game_paused = new_pause_status
+
+    def is_game_over(self):
+        return self.game_over
+
+    def get_toothbrush_status(self):
+        return self.toothbrush
+
+    def set_toothbrush_status(self, new_brush_status):
+        self.toothbrush = new_brush_status
+
+    def get_toothpaste_status(self):
+        return self.toothpaste
+
+    def set_toothpaste_status(self, new_paste_status):
+        self.toothpaste = new_paste_status
